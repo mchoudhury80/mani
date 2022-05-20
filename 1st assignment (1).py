7bd7089ebe7727c07fa323b6d7ce225d93a38df4
mchoudhury80 <105153195+mchoudhury80@users.noreply.github.com>
2022-05-20 11:21:40 -0400

    1st assignment(5/14)

---
+++ 1st assignment (1).py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s = "This is my First Python programming class and i am learning python string and its function"
+
+
+# In[9]:
+
+
+s[1 : 300 :3 ]
+
+
+# In[24]:
+
+
+p ="Python is interesting"
+
+
+# In[25]:
+
+
+p[: : -1]
+
+
+# In[26]:
+
+
+k ="Krishnesi is a very good girl"
+
+
+# In[27]:
+
+
+k.swapcase().split()
+
+
+# In[28]:
+
+
+r ="rajesh is handsome man"
+
+
+# In[33]:
+
+
+r.capitalize()
+
+
+# In[35]:
+
+
+a = '123' 
+
+
+# In[36]:
+
+
+a.isalpha()
+
+
+# In[40]:
+
+
+n = '1234siva8765'
+
+
+# In[41]:
+
+
+n.isalnum()
+
+
+# In[48]:
+
+
+e ='1234\tabc\t1xyz'
+
+
+# In[49]:
+
+
+e.expandtabs()
+
+
+# In[50]:
+
+
+st = "   Tiger is a wild animal    "
+
+
+# In[51]:
+
+
+st.strip()
+
+
+# In[52]:
+
+
+stl = "    I love dog  "
+
+
+# In[53]:
+
+
+stl.lstrip()
+
+
+# In[54]:
+
+
+str = "    I love cat too     "
+
+
+# In[55]:
+
+
+str.rstrip()
+
+
+# In[63]:
+
+
+re = "My name is vasu"
+
+
+# In[64]:
+
+
+re.replace("vasu" , "mani")
+
+
+# In[66]:
+
+
+center ="This is to make string center"
+center.center(50,"*")
+
+
+# In[4]:
+
+
+### ans 11-Compiler generates intermediatery machine code,later this machin code run in specfic virtual environment .for example javac generaintermediatery machine code and java run runs in virtual environentInterpreter generates internally machine code ans run in the virtual environment ,for ex python.
+    ####
+
+
+# In[5]:
+
+
+#### ans 12- Python is an interpreterted language, here we directly run the code but internally it generates binary code and run in the pyhon virtual environment. like eneconda.
+####
+
+
+# In[ ]:
+
+
+###ans 13- Python is a suitable language for data science projects along with machine learning. bussiness intelgency projects for example tablo . also we can use python for full stack developers using Django/Flask. ###
+
